7049eb1e87983d344390b06f2993cf4db531e643
elghanbibadr <bghanbi50@gmail.com>
2024-01-20 13:03:39 +0100

    remove a team membre

--- components/Team/teamData.tsx
+++ components/Team/teamData.tsx
@@ -9,16 +9,16 @@ const teamData: Card[] = [
     paragraph:
       "The founding member Jeffrey boasts a wealth of professional experience spanning over 16 years in online marketing and investment. Jeffrey embarked on his entrepreneurial journey before he was a teenager, establishing his first online business. He has a Bachelor of Science in Business, minor in Marketing, and a Master of Business Administration (MBA) in Business Management, concentration in Finance.",
   },
+  // {
+  //   id: 1,
+  //   image: "/images/snakedice/team/team2.png",
+  //   name: "Masao Ito",
+  //   title: "Lead Developer",
+  //   paragraph:
+  //     "He holds a Bachelor’s degree in Computer Science and has been passionate about coding since elementary school, setting the stage for a remarkable career. He is a full stack (including blockchain, FE, BE) developer with proficiency in nearly every programming language. His official title is Core Team - Lead Developer, where he will play a pivotal role in the technical aspects of our venture.",
+  // },
   {
     id: 1,
-    image: "/images/snakedice/team/team2.png",
-    name: "Masao Ito",
-    title: "Lead Developer",
-    paragraph:
-      "He holds a Bachelor’s degree in Computer Science and has been passionate about coding since elementary school, setting the stage for a remarkable career. He is a full stack (including blockchain, FE, BE) developer with proficiency in nearly every programming language. His official title is Core Team - Lead Developer, where he will play a pivotal role in the technical aspects of our venture.",
-  },
-  {
-    id: 2,
     image: "/images/snakedice/team/team3.png",
     name: "Odera Ogbuehi",
     title: "Social Media Director",
@@ -26,7 +26,7 @@ const teamData: Card[] = [
       "He holds a degree in Law, showcasing a strong foundation in legal expertise. With a profound passion for social media marketing and over 4 years of hands-on experience, Odera has been instrumental in helping organizations enhance their online presence. As a dynamic innovator, he will spearhead the management of marketing strategies and community engagement with the aim of contributing significantly to the growth and success of the project.",
   },
   {
-    id: 3,
+    id: 2,
     image: "/images/snakedice/team/team4.png",
     name: "Marko Bijelic",
     title: "UX/UI & Graphic Design",
